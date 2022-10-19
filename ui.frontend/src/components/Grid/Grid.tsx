0b9315c8f12a415075a7c38efb512b1648ad0c04
Luan Leal <luan.leal@compasso.com.br>
2022-10-19 10:55:19 -0300

    feat: about section responsiveness finished

--- ui.frontend/src/components/Grid/Grid.tsx
+++ ui.frontend/src/components/Grid/Grid.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Grid.scss";
 
-export const Grid = ({ children, className, style, ...props }) => {
+export const Grid = ({ children, className, style, ...props }: any) => {
   return (
     <div className={`grid--container ${className}`} style={style} {...props}>
       <div className="grid--inner">{children}</div>
--- ui.frontend/src/components/ReactAbout/index.jsx
+++ ui.frontend/src/components/ReactAbout/index.jsx
@@ -1,51 +1,54 @@
 import { MapTo } from "@adobe/aem-react-editable-components";
 import React from "react";
 import Row from "../microcomponent/TitleRow";
+import { Grid } from "../Grid/Grid";
 
 import "./index.scss";
 
 const About = ({ title }) => {
   return (
-    <section id="aboutSection">
-      <div className="title">
-        <h2>
-          {title}
-          <Row />
-        </h2>
-      </div>
+    <Grid>
+      <section id="aboutSection">
+        <div className="title">
+          <h2>
+            {title}
+            <Row />
+          </h2>
+        </div>
 
-      <div>
-        <p>
-          Cycling is the healthiest and most sustainable way to get around. And
-          there are large bike-friendly cities across the globe. But “car
-          culture” is still much bigger and stronger. So, we thought, what if
-          riding a bike had an extra reward? And what if we pointed a direction,
-          or showed the north, for this means of transportation to encourage a
-          change in behavior?
-        </p>
-        <br />
-        <p>
-          <span>What if cycling could be even more valuable?</span>
-        </p>
-        <br />
-        <p>
-          To answer that question, we came up with the
-          <span>Cryptobike by Compass</span> . A platform using blockchain
-          technology to{" "}
-          <span> transform your cycling data into unique NFTs.</span>
-        </p>
-        <br />
-        <p>
-          When you reach milestones,
-          <span>
-            you will earn exclusive NFTs with your cycling information.
-          </span>
-          Distance, speed, timing: all in your own digital art.
-        </p>
-        <br />
-        <p>From now on, riding a bike has become even more valuable.</p>
-      </div>
-    </section>
+        <div>
+          <p>
+            Cycling is the healthiest and most sustainable way to get around.
+            And there are large bike-friendly cities across the globe. But “car
+            culture” is still much bigger and stronger. So, we thought, what if
+            riding a bike had an extra reward? And what if we pointed a
+            direction, or showed the north, for this means of transportation to
+            encourage a change in behavior?
+          </p>
+          <br />
+          <p>
+            <span>What if cycling could be even more valuable?</span>
+          </p>
+          <br />
+          <p>
+            To answer that question, we came up with the
+            <span>Cryptobike by Compass</span> . A platform using blockchain
+            technology to{" "}
+            <span> transform your cycling data into unique NFTs.</span>
+          </p>
+          <br />
+          <p>
+            When you reach milestones,
+            <span>
+              you will earn exclusive NFTs with your cycling information.
+            </span>
+            Distance, speed, timing: all in your own digital art.
+          </p>
+          <br />
+          <p>From now on, riding a bike has become even more valuable.</p>
+        </div>
+      </section>
+    </Grid>
   );
 };
 
