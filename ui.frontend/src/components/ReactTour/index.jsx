693ea48e69b17fb7caae1c7cb7ef72ba02297f4c
Luan Leal <luan.leal@compasso.com.br>
2022-10-19 12:23:10 -0300

    feat: tour section responsiveness finished

--- ui.frontend/src/components/ReactTour/index.jsx
+++ ui.frontend/src/components/ReactTour/index.jsx
@@ -3,39 +3,39 @@ import React from "react";
 import Row from "../microcomponent/TitleRow";
 
 import "./index.scss";
+import { Grid } from "./../Grid/Grid";
 
 function Tour({ title }) {
   return (
-    <section id="TourSection">
-      <div className="left">
-        <div>
-          <h2>{title}</h2>
-          <Row></Row>
+    <Grid id="TourSection">
+      <section>
+        <div className="left">
+          <div>
+            <h2>{title}</h2>
+            <Row></Row>
+          </div>
+          <div>
+            <p>
+              The world's most famous cycling events are known as “Tours”.
+              There's a famous one in France. However, like every competition,
+              they only last for a short time and our goal is to go beyond that.
+              <span>
+                {" "}
+                We want to invite the whole world to turn cycling into a habit.
+              </span>{" "}
+              So, we created Tour de Terre. A challenge that invites everyone to
+              ride for the same team:
+              <span> the Earth. </span>
+              <br /> <br />
+              If in the most famous tours in the world athletes ride for their
+              teams, in the Tour de Terre <span>we ride together</span> for the
+              planet.
+            </p>
+          </div>
         </div>
-        <div>
-          <p>
-            The world's most famous cycling events are known as “Tours”. <br />
-            There's a famous one in France. However, like every <br />
-            competition, they only last for a short time and our goal is to go
-            <br />
-            beyond that.
-            <span>
-              {" "}
-              We want to invite the whole world to turn <br /> cycling into a
-              habit.{" "}
-            </span>
-            So, we created Tour de Terre. A challenge <br />
-            that invites everyone to ride for the same team:
-            <span> the Earth.</span> <br /> <br />
-            If in the most famous tours in the world athletes ride for their
-            <br />
-            teams, in the Tour de Terre <span>we ride together</span> for the
-            planet.
-          </p>
-        </div>
-      </div>
-      <div className="right"></div>
-    </section>
+        <div className="right"></div>
+      </section>
+    </Grid>
   );
 }
 
